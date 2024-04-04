f2958f54679a818fe5766c6e17752885968903a8
Quinn7089 <Quinn7089j@gmail.com>
2024-04-04 09:58:52 -0500

    [1.09] Create TicketFile constructor to read data.txt

--- Program.cs
+++ Program.cs
@@ -280,112 +280,140 @@ public class TicketFile
 
     public List<Enhancment> enhancments { get; set; }
 
-}
-
-
-
-
-
-
-
-
-
-
-
-public class Ticket
-{
-
-
-    public Ticket(string ticketId, string summary, string status, string priority, string submitter, string assigend, string watching)
-    {
-        TicketID = ticketId;
-        Summary = summary;
-        Status = status;
-        Priority = priority;
-        Submitter = submitter;
-        Assigend = assigend;
-        Watching = watching;
-    }
-
-    public string TicketID { get; set; }
-
-    public string Summary { get; set; }
-
-    public string Status { get; set; }
-
-    public string Priority { get; set; }
-
-    public string Submitter { get; set; }
-
-    public string Assigend { get; set; }
-
-    public string Watching { get; set; }
-
-    public string Software { get; set; }
-
-    public string Cost { get; set; }
-
-    public string estimate { get; set; }
-
-
-}
-
-public class Enhancment : Ticket
-{
-
-    public Enhancment(string ticketId, string summary, string status, string priority, string submitter, string assigend, string watching, string software, string cost, string estimate) : base(ticketId, summary, status, priority, submitter, assigend, watching)
+    public TicketFile(string ticketFile, string EnhancementFile, string taskFile)
     {
 
-        TicketID = ticketId;
-        Summary = summary;
-        Status = status;
-        Priority = priority;
-        Submitter = submitter;
-        Assigend = assigend;
-        Watching = watching;
-        Software = software;
-        Cost = cost;
-        Estimate = estimate;
+        filePath = ticketFile;
+        tickets = new List<Ticket>();
+        // enhancmentPath = EnhancementFile;
+        enhancments = new List<Enhancment>();
+        tasks = new List<Task>();
+        try
+        {
+            StreamReader sr = new StreamReader(filePath);
+            while (!sr.EndOfStream)
+            {
 
+                string ticketData = sr.ReadLine();
+
+                string[] ticketDataArray = ticketData.Split(",");
+
+                Ticket newTicket = new Ticket(ticketDataArray[0], ticketDataArray[1], ticketDataArray[2], ticketDataArray[3], ticketDataArray[4], ticketDataArray[5], ticketDataArray[6]);
+
+                tickets.Add(newTicket);
+
+            }
+        }
+        catch
+        {
+
+        }
     }
 
 
 
-    public string Software { get; set; }
-
-    public string Cost { get; set; }
-
-    public string Estimate { get; set; }
 
 
 
 
-}
 
 
 
 
-public class Task : Ticket
-{
-
-
-    public Task(string ticketId, string summary, string status, string priority, string submitter, string assigend, string watching, string name, string dueDate) : base(ticketId, summary, status, priority, submitter, assigend, watching)
+    public class Ticket
     {
-        TicketID = ticketId;
-        Summary = summary;
-        Status = status;
-        Priority = priority;
-        Submitter = submitter;
-        Assigend = assigend;
-        Watching = watching;
-        Name = name;
-        DueDate = dueDate;
+
+
+        public Ticket(string ticketId, string summary, string status, string priority, string submitter, string assigend, string watching)
+        {
+            TicketID = ticketId;
+            Summary = summary;
+            Status = status;
+            Priority = priority;
+            Submitter = submitter;
+            Assigend = assigend;
+            Watching = watching;
+        }
+
+        public string TicketID { get; set; }
+
+        public string Summary { get; set; }
+
+        public string Status { get; set; }
+
+        public string Priority { get; set; }
+
+        public string Submitter { get; set; }
+
+        public string Assigend { get; set; }
+
+        public string Watching { get; set; }
+
+        public string Software { get; set; }
+
+        public string Cost { get; set; }
+
+        public string estimate { get; set; }
+
+
     }
 
-    public string Name { get; set; }
-    public string DueDate { get; set; }
+    public class Enhancment : Ticket
+    {
+
+        public Enhancment(string ticketId, string summary, string status, string priority, string submitter, string assigend, string watching, string software, string cost, string estimate) : base(ticketId, summary, status, priority, submitter, assigend, watching)
+        {
+
+            TicketID = ticketId;
+            Summary = summary;
+            Status = status;
+            Priority = priority;
+            Submitter = submitter;
+            Assigend = assigend;
+            Watching = watching;
+            Software = software;
+            Cost = cost;
+            Estimate = estimate;
+
+        }
+
+
+
+        public string Software { get; set; }
+
+        public string Cost { get; set; }
+
+        public string Estimate { get; set; }
 
 
 
 
-}
+    }
+
+
+
+
+    public class Task : Ticket
+    {
+
+
+        public Task(string ticketId, string summary, string status, string priority, string submitter, string assigend, string watching, string name, string dueDate) : base(ticketId, summary, status, priority, submitter, assigend, watching)
+        {
+            TicketID = ticketId;
+            Summary = summary;
+            Status = status;
+            Priority = priority;
+            Submitter = submitter;
+            Assigend = assigend;
+            Watching = watching;
+            Name = name;
+            DueDate = dueDate;
+        }
+
+        public string Name { get; set; }
+        public string DueDate { get; set; }
+
+
+
+
+    }
